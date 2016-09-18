7b46a9ee5539fffcd813a92e1003909814043876
Shirish Goyal <shirish.goyal@gmail.com>
2016-09-18 05:35:53 -0700

    theme config

--- frontend/src/app/theme/theme.config.js
+++ frontend/src/app/theme/theme.config.js
@@ -3,28 +3,30 @@
  */
 
 (function () {
-  'use strict';
+    'use strict';
 
-  angular.module('BlurAdmin.theme')
-    .config(config);
+    angular.module('BlurAdmin.theme')
+        .config(config);
 
-  /** @ngInject */
-  function config($httpProvider, $compileProvider, baConfigProvider, colorHelper) {
-    $httpProvider.interceptors.push('AuthHttpResponseInterceptor');
-    
-    $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+    /** @ngInject */
+    function config($httpProvider, $compileProvider, baConfigProvider, colorHelper) {
+        $httpProvider.interceptors.push('AuthHttpResponseInterceptor');
+
+        $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
         $httpProvider.defaults.xsrfCookieName = 'csrftoken';
 
-     $compileProvider.imgSrcSanitizationWhitelist(/^\s*(http|blob|data):/);
+        $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|chrome-extension):/);
 
-    //baConfigProvider.changeTheme({blur: true});
+        $compileProvider.imgSrcSanitizationWhitelist(/^\s*(https?|local|data|chrome-extension):/);
 
-    // baConfigProvider.changeColors({
-    //  default: 'rgba(#000000, 0.2)',
-    //  defaultText: '#ffffff',
-    //  dashboard: {
-    //    white: '#ffffff',
-    //  },
-    // });
-  }
+        //baConfigProvider.changeTheme({blur: true});
+
+        // baConfigProvider.changeColors({
+        //  default: 'rgba(#000000, 0.2)',
+        //  defaultText: '#ffffff',
+        //  dashboard: {
+        //    white: '#ffffff',
+        //  },
+        // });
+    }
 })();
