8329fa8761edef01d600bc4808fc76024e1003b2
Shirish Goyal <shirish.goyal@gmail.com>
2016-09-25 03:56:29 -0700

    paged response

--- scholars/courses/viewsets.py
+++ scholars/courses/viewsets.py
@@ -34,10 +34,10 @@ class CourseViewSet(viewsets.ModelViewSet):
     def list(self, request, *args, **kwargs):
         queryset = self.filter_queryset(self.get_queryset())
 
-        # page = self.paginate_queryset(queryset)
-        # if page is not None:
-        #     serializer = self.get_serializer(page, many=True)
-        #     return self.get_paginated_response(serializer.data)
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True, fields=('id', 'name', 'status', 'owner'))
+            return self.get_paginated_response(serializer.data)
 
         serializer = self.get_serializer(queryset, many=True, fields=('id', 'name', 'status', 'owner'))
         return Response(serializer.data)
